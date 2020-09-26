2c6010ce622b940f7d86286100da13ebd59dbdd0
amine <jait.amine9@gmail.com>
2020-09-26 14:37:46 +0100

    added the sections

--- src/app/section3/section3.component.ts
+++ src/app/section3/section3.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/services/userService.service';
 
 @Component({
   selector: 'app-section3',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Section3Component implements OnInit {
 
-  constructor() { }
+  constructor(private userService : UserService, private router : Router) { }
 
   ngOnInit() {
+    let isConnected =this.userService.getConnected();
+    console.log(isConnected);
+
+    if(isConnected === undefined) this.router.navigate(["/signup"])
+
   }
 
 }
